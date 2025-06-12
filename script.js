86501cbb7dfd8869b3afb45231c4cedff3361f92
Mukesh Patil <mspatil.jalgaon@gmail.com>
2025-06-12 16:52:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+  const menu = document.getElementById('epMenu');
+  menu.classList.toggle('active');
+}
+// Toggle submenus in mobile view
+document.addEventListener('DOMContentLoaded', function () {
+  const parentItems = document.querySelectorAll('.ep_tm_menu > li');
+
+  parentItems.forEach(item => {
+    item.addEventListener('click', function (e) {
+      if (window.innerWidth <= 970 && item.querySelector('ul')) {
+        e.stopPropagation();
+        e.preventDefault();
+        item.classList.toggle('active');
+      }
+    });
+  });
+});
